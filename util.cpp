ee24e5f3d4748249fcc378b528d0792ddc836fa6
Sebastian Nesgaard Jensen <snje@dtu.dk>
2015-11-13 16:01:01 +0100

    Added logging and printing utility

---
+++ util.cpp
@@ -0,0 +1,3 @@
+#include "util.h"
+
+bool verbose = true;
---
+++ util.h
@@ -0,0 +1,29 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <syslog.h>
+
+//extern bool verbose_flag;
+
+#define debug(M, ...) printf("DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define info(M, ...) {\
+  syslog(LOG_INFO, "INFO %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
+  printf("INFO %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
+}
+#define warn(M, ...) {
+  syslog(LOG_WARNING, "WARN %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
+  printf("WARN %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
+}
+#define error(M, ...) syslog(LOG_ERR, "ERROR %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define errcheck(E) if(E) {printf("Error at %s:%d: code = %i\n", __FILE__, __LINE__, E);}
+#define errguard(E, F) if(!E) {E = (F); errcheck(E);}
+
+inline int idivup(int a, int b) {
+    return (a % b != 0) ? (a / b + 1) : (a / b);
+}
+
+#endif // __UTIL_H__
